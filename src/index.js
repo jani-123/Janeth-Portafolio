46e61ac28d7b84a86c991f9dda1ad9973cd004f2
jani-123 <janina.rqa@gmail.com>
2017-12-04 12:08:28 -0500

    subiendo cambios gh-pages

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route , Redirect} from 'react-router-dom';
 import { Provider } from "redux-zero/react";
 import store from "./store";
+import { Redirect } from '../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-router';
 
 const Index = () => (
   <Provider store={store}>
     <HashRouter>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" render={() => <App />} />
+        <Route  path="/Janeth-Portafolio" render={ () => <Redirect to="/"/>}/>
       </Switch>
     </HashRouter>
   </Provider>
