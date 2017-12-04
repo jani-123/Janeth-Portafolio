d1e770635bb655369847e0ae0d8c97259114e4b2
jani-123 <janina.rqa@gmail.com>
2017-12-04 12:09:47 -0500

    subiendo cambios gh-pages2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { HashRouter, Switch, Route , Redirect} from 'react-router-dom';
 import { Provider } from "redux-zero/react";
 import store from "./store";
-import { Redirect } from '../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/react-router';
+
 
 const Index = () => (
   <Provider store={store}>
